7d511fa040687b9ae03c4e45c2d975a3dc9ba511
Macostik <yura-granchenko@yandex.ru>
2023-12-22 19:17:33 +0200

    feat: init project

---
+++ CGX_Myndfull_Test_Task/CGX_Myndfull_Test_Task/CGX_Myndfull_Test_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CGX_Myndfull_Test_TaskApp.swift
+//  CGX_Myndfull_Test_Task
+//
+//  Created by Macostik on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CGX_Myndfull_Test_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CGX_Myndfull_Test_Task/CGX_Myndfull_Test_Task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CGX_Myndfull_Test_Task
+//
+//  Created by Macostik on 22.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
